2a371663c4af863608b2bb7a5843dac16b6a7896
devaraj46 <62149855+devaraj93@users.noreply.github.com>
2021-11-08 10:34:39 +0530

    filter&pagination

--- Assignment1/table.js
+++ Assignment1/table.js
@@ -1,7 +1,16 @@
 "use strict";
 
-let duplicatearraydata;
+// let duplicatearraydata = [];
 let originalData;
+var duplicatedata = [];
+
+let tablerowlen = 0;
+let tablerowdata = [];
+
+var table = document.querySelector(".pagination");
+
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
 
 const responseData = async () => {
   try {
@@ -14,10 +23,9 @@ const responseData = async () => {
   }
 };
 
-const getTableData = async () => {
+const convertResponse = async () => {
   originalData = await responseData();
   const duplicateN = 3;
-  var duplicatedata = [];
   var k = 1;
   if (originalData.isAuthError === true) {
     for (let i = 0; i < duplicateN; i++) {
@@ -34,43 +42,82 @@ const getTableData = async () => {
     errormtag.textContent = originalData.error.message;
     document.body.appendChild(errormtag);
   }
-  return duplicatedata;
+  //return duplicatedata;
 };
 
-const createTable = async () => {
-  duplicatearraydata = await getTableData();
-
-  var table = document.querySelector(".pagination");
-  for (let i = 0; i < document.getElementById("rows").value; i++) {
+const createTableRows = async () => {
+  // duplicatearraydata =
+  await convertResponse();
+  for (let i = 0; i < duplicatedata.length; i++) {
     var tr = document.createElement("tr");
-    table.appendChild(tr);
-    for (var j = 0; j < duplicatearraydata[i].length; j++) {
+    for (var j = 0; j < duplicatedata[i].length; j++) {
       var tdElement = document.createElement("td");
-      tdElement.innerHTML = duplicatearraydata[i][j];
+      tdElement.innerHTML = duplicatedata[i][j];
       tr.appendChild(tdElement);
     }
+    tablerowdata.push(tr);
   }
 };
 
-document.getElementById("rows").addEventListener("change", createTable);
+async function setDataOnLoad() {
+  await createTableRows();
+  for (let i = tablerowlen; i < tablerowlen + x; i++) {
+    table.appendChild(tablerowdata[i]);
+  }
+}
+
+setDataOnLoad();
+
+var x = document.getElementById("rows").value;
+
+// rows.addEventListener("change", () => {
+//   x = document.getElementById("rows").value;
+//   console.log(x);
+// });
+
+next.addEventListener("click", () => {
+  tablerowlen == tablerowdata.length - 5
+    ? (tablerowlen = 0)
+    : (tablerowlen += 5);
+  table.innerHTML = "";
+  for (let i = tablerowlen; i < tablerowlen + 5; i++) {
+    table.appendChild(tablerowdata[i]);
+  }
+});
+
+previous.addEventListener("click", () => {
+  tablerowlen == 0
+    ? (tablerowlen = tablerowdata.length - 5)
+    : (tablerowlen -= 5);
+  table.innerHTML = "";
+  for (let i = tablerowlen; i < tablerowlen + 5; i++) {
+    table.appendChild(tablerowdata[i]);
+  }
+});
 
 function searchFilter() {
-  var tr, searchvalue, textValue;
+  var searchvalue, textValue;
+  table.innerHTML = "";
   searchvalue = document
     .getElementById("myInput")
     .value.toUpperCase()
     .replace(/ /g, "");
-  tr = document.getElementsByTagName("tr");
-
-  for (let i = 0; i < tr.length; i++) {
-    textValue = tr[i]
+  for (let i = 0; i < tablerowdata.length; i++) {
+    textValue = tablerowdata[i]
       .getElementsByTagName("td")[1]
       .textContent.toUpperCase()
       .replace(/ /g, "");
     if (textValue.indexOf(searchvalue) > -1) {
-      tr[i].style.display = "";
+      console.log(textValue);
+      console.log(searchvalue);
+      table.appendChild(tablerowdata[i]);
+      // tr[i].style.display = "";
     } else {
-      tr[i].style.display = "none";
+      // tr[i].style.display = "none";
     }
+  }
+  if (searchvalue == 0) {
+    table.innerHTML = "";
+    setDataOnLoad();
   }
 }
